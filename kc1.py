ccab6c25e54f31acd396036bacea1798f8837675
502bjm <122325987+502bjm@users.noreply.github.com>
2023-02-19 22:19:14 -0500

    Add files via upload

---
+++ kc1.py
@@ -0,0 +1,14 @@
+# Print "Hello World!"
+print("Hello World!")
+
+# List populated with several values
+my_list = [1, 2, 3.14, "apple"]
+print(my_list[2]) # Prints 3.14
+
+# Dictionary populated with two keys and two values
+my_dict = {"key1": "value1", "key2": "value2"}
+print(my_dict["key1"]) # Prints "value1"
+
+# Tuple with 4 values
+my_tuple = (10, 20, 30, 40)
+print(my_tuple[2]) # Prints 30
